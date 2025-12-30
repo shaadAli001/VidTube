96ad43bfcd361236262e9e1f17bf7c58472ed6a2
shaadAli001 <alishaad908@gmail.com>
2025-12-30 14:06:07 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let menuIcon = document.querySelector(".menu-icon");
 let sidebar = document.querySelector(".sidebar");
+let container = document.querySelector(".container");
 
 menuIcon.addEventListener("click", () => {
-    sidebar.classList.toggle("small-sidebar");
+  sidebar.classList.toggle("small-sidebar");
+  container.classList.toggle("large-contianer");
 });
